1de2b4a769c0d008ca47d6ab269c1c4b50dc02a2
sebaszeledon <sebas.zeledon@gmail.com>
2024-04-23 21:18:08 -0600

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.7.0/ethers.min.js";
+import { abi, contractAddress } from "./constants.js";
 
 const connectButton = document.getElementById("connectButton");
 const fundButton = document.getElementById("fundButton");
@@ -8,16 +9,26 @@ fundButton.onclick = fund;
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
     await window.ethereum.request({method : "eth_requestAccounts"});
-    document.getElementById("connectButton").innerHTML = "Connected!";
+    connectButton.innerHTML = "Connected!";
     } else {
-        document.getElementById("connectButton").innerHTML = "Please install Metamask";
+        connectButton.innerHTML = "Please install Metamask";
     }
 }
 
-async function fund(ethAmount) {
+async function fund() {
+    const ethAmount = "1";
     console.log(`Funding with ${ethAmount}...`);
     if (typeof window.ethereum !== "undefined") {
         // provider / connection to the blockchain
+        // signer / wallet / someone with some gas
+        // contract that we are interacting with
+        // ^ ABI & Address
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+        const transactionResponse = await contract.fund({
+            value: ethers.parseEther(ethAmount),
+        }); // 13:18:25
     }
 }
 
