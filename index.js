727a481e9d6e8f57c8f720e3fcf61f6b6b326824
sebaszeledon <sebas.zeledon@gmail.com>
2024-04-23 20:28:50 -0600

    update

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.7.0/ethers.min.js";
+
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
     await window.ethereum.request({method : "eth_requestAccounts"});
@@ -6,3 +13,15 @@ async function connect() {
         document.getElementById("connectButton").innerHTML = "Please install Metamask";
     }
 }
+
+async function fund(ethAmount) {
+    console.log(`Funding with ${ethAmount}...`);
+    if (typeof window.ethereum !== "undefined") {
+        // provider / connection to the blockchain
+    }
+}
+
+//fund function
+
+
+//withdraw
